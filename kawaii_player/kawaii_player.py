f6867f2835c20b094fe2ef89c35eb4e5f6964c54
kanishka-linux <gnulinux.fan.10@gmail.com>
2017-02-20 10:41:34 +0530

    version number update

--- kawaii_player/kawaii_player.py
+++ kawaii_player/kawaii_player.py
@@ -8103,7 +8103,7 @@ class Ui_MainWindow(object):
 		self.mplayer_timer.timeout.connect(self.mplayer_unpause)
 		self.mplayer_timer.setSingleShot(True)
 		#self.frame_timer.start(5000)
-		self.version_number = (4,2,0,0)
+		self.version_number = (1,1,0,0)
 		self.threadPool = []
 		self.threadPoolthumb = []
 		self.thumbnail_cnt = 0
