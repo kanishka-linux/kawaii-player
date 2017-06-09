0cf02bfd7a213a5b953842434ce9043ed4bab35a
kanishka-linux <kanishka.linux@gmail.com>
2017-06-09 08:36:09 +0530

    addons update

--- kawaii_player/Plugins/Shoutcast.py
+++ kawaii_player/Plugins/Shoutcast.py
@@ -1,3 +1,18 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import sys
 import urllib
 import pycurl
@@ -50,7 +65,7 @@ class Shoutcast():
 		for i in l:
 			try:
 				#print(i['ID'],i['Name'],i['Bitrate'],i['Listeners'])
-				s.append(i['Name'].replace('/','-')+'	id='+str(i['ID'])+'\nBitrate='+str(i['Bitrate'])+'\nListeners='+str(i['Listeners'])+'\n')
+				s.append(i['Name'].replace('/','-')+'	id='+str(i['ID'])+'|Bitrate='+str(i['Bitrate'])+'|Listeners='+str(i['Listeners']))
 			except:
 				pass
 		return s
@@ -132,7 +147,7 @@ class Shoutcast():
 		return m
 	
 	def getEpnList(self,name,opt,depth_list,extra_info,siteName,category):
-		name_id = (re.search('id=[^\n]*',extra_info).group()).split('=')[1]
+		name_id = (re.search('id=[^|]*',extra_info).group()).split('=')[1]
 		#nm = name.rsplit('-',1)
 		#name = nm[0]
 		#name_id = nm[1]
