d9f1c069de96803b997d9119c81268e87a207cda
kanishka-linux <kanishka.linux@gmail.com>
2017-06-09 08:36:21 +0530

    addons update

--- kawaii_player/Plugins/Torrent.py
+++ kawaii_player/Plugins/Torrent.py
@@ -15,9 +15,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with kawaii-player.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 """
 
 
