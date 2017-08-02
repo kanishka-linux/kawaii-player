06359956e3d6d63198724f6813ec1f146ac14d19
kanishka-linux <kanishka.linux@gmail.com>
2017-08-03 00:47:08 +0530

    comments removed

--- kawaii_player/music_artist.py
+++ kawaii_player/music_artist.py
@@ -15,29 +15,16 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with kawaii-player.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 """
-import sys
-import urllib
-import pycurl
-from io import StringIO, BytesIO
-import re
-import subprocess
-import os.path
-from subprocess import check_output
+
 from bs4 import BeautifulSoup
-import random
-from os.path import expanduser
-import datetime
-from player_functions import ccurl, naturallysorted
+from player_functions import ccurl
 
 class MusicArtist():
-    
+
     def __init__(self):
         self.hdr = ''
-        
+
     def getContentUnicode(self, content):
         if isinstance(content, bytes):
             print("I'm byte")
@@ -50,62 +37,45 @@ class MusicArtist():
             content = str(content)
             print("I'm unicode")
         return content
-        
-    def ccurlT(self, url, rfr):
-        content = ccurl(url)
-        return content
-        
+
     def search(self, name, name_url):
         if not name_url:
             name = name.replace(' ', '+')
             url = "http://www.last.fm/search?q="+name
         else:
             url = name_url
-        #print(url)
         wiki = ""
         if not name_url:
-            #content = self.ccurl(url, '')
-            #print content
             content = ccurl(url)
             soup = BeautifulSoup(content, 'lxml')
 
             link = soup.findAll('div', {'class':'row clearfix'})
-            #print link
             for i in link:
                 j = i.findAll('a')
-                #print (j)
                 for k in j:
                     try:
                         url = k['href']
-                        name = k.text
-                        break
+                        if '?q=' not in url:
+                            logger.info(url)
+                            name = k.text
+                            break
                     except:
                         pass
-            #print(name)
-            #print(url)
-        
-        
+
         if url.startswith('http'):
             url = url
         else:
             url = "http://www.last.fm" + url
-        
-        #print(url, '--music-artist--')
+
         img_url = url+'/+images'
         wiki_url = url + '/+wiki'
-        #print(wiki_url)
-        #content = self.ccurl(wiki_url, '')
         content = ccurl(wiki_url)
         soup = BeautifulSoup(content, 'lxml')
         link = soup.find('div', {'class':'wiki-content'})
-        #print(link)
 
         if link:
-            
             wiki = link.text
-            #print wiki
 
-        #content = self.ccurl(img_url, '')
         content = ccurl(img_url)
         soup = BeautifulSoup(content, 'lxml')
         link = soup.findAll('ul', {'class':'image-list'})
@@ -119,7 +89,6 @@ class MusicArtist():
                 u = u1 + '/770x0/'+u2
                 img.append(u)
         img = list(set(img))
-        #print img
         print(len(img))
         img.append(wiki)
         return img
