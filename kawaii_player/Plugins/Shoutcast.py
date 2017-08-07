e59608eac25c86341a1dc3e32b9016ff56cca843
kanishka-linux <kanishka.linux@gmail.com>
2017-08-07 13:55:54 +0530

    code cleaning

--- kawaii_player/Plugins/Shoutcast.py
+++ kawaii_player/Plugins/Shoutcast.py
@@ -13,27 +13,20 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys
-import urllib
-import pycurl
-from io import StringIO, BytesIO
-import re
-import random
-import subprocess
-from subprocess import check_output
-from bs4 import BeautifulSoup
 import os
-import os.path
-from subprocess import check_output
-import shutil
+import sys
+import re
 import json
-from player_functions import ccurl, naturallysorted
+from bs4 import BeautifulSoup
+from player_functions import ccurl
 
 
 class Shoutcast():
+    
     def __init__(self, tmp):
         self.hdr = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:45.0) Gecko/20100101 Firefox/45.0'
         self.tmp_dir = tmp
+        
     def getOptions(self):
             criteria = ['History', 'Genre']
             return criteria
@@ -43,13 +36,10 @@ class Shoutcast():
         
     def process_page(self, content):
         content = re.sub(r'\\', "-", content)
-        #print(content)
-        #f = open('/tmp/tmp.txt', 'w')
-        #f.write(content)
-        #f.close()
         try:
             l = json.loads(content)
-        except:
+        except Exception as err:
+            print(err, '--49--')
             o = re.findall('{[^}]*}', content)
             l = []
             for i in o:
@@ -58,16 +48,14 @@ class Shoutcast():
                     j = json.loads(i)
                     print(j['ID'], j['Name'])
                     l.append(j)
-                except:
-                    pass
-                    print('----------------------error---------------')
+                except Exception as err:
+                    print(err, '--58--')
         s = []
         for i in l:
             try:
-                #print(i['ID'], i['Name'], i['Bitrate'], i['Listeners'])
                 s.append(i['Name'].replace('/', '-')+'	id='+str(i['ID'])+'|Bitrate='+str(i['Bitrate'])+'|Listeners='+str(i['Listeners']))
-            except:
-                pass
+            except Exception as err:
+                print(err, '--64--')
         return s
         
     def search(self, name):
@@ -77,19 +65,17 @@ class Shoutcast():
             m = self.getCompleteList(name.upper(), 1)
         else:
             url = "https://www.shoutcast.com/Home/BrowseByGenre"
-            #content = ccurl(url, name, 1)
             post = "genrename="+name
             content = ccurl(url+'#'+'-d'+'#'+post)
             m = self.process_page(content)
         return m
         
     def getCompleteList(self, opt, genre_num):
+        m = []
         if opt == 'Genre' and genre_num == 0:
             url = "http://www.shoutcast.com/"
-            #content = ccurl(url, "", 1)
             content = ccurl(url)
             m = re.findall('Genre[^"]name[^"]*', content)
-            #print m
             j = 0
             for i in m:
                 m[j] = re.sub('Genre[^"]name=', '', i)
@@ -97,49 +83,14 @@ class Shoutcast():
                 j = j+1
             m.sort()
             print(m)
-            #n = ["History", "Genre", "TV"]
             n = ["History", "Genre"]
             m = n + m
         elif opt == 'History':
-            a =0
+            pass
         elif opt == 'TV':
-            name = []
-            track = []
-            aformat = []
-            listeners = []
-            bitrate = []
-            idr = []
-            url = "http://thugie.nl/streams.php"
-            #content = ccurl(url, "", 4)
-            content = ccurl(url)
-            soup = BeautifulSoup(content, 'lxml')
-            tmp = soup.prettify()
-            #m = soup.findAll('div', {'class':'boxcenterdir fontstyle'})
-            #soup = BeautifulSoup(tmp, 'lxml')
-            m = []
-            links = soup.findAll('div', {'class':'dirOuterDiv1 clearFix'})
-            for i in links:
-                j = i.findAll('a')
-                q = i.find_next('h2')
-                g = i.find_next('h4')
-                z = g.find_next('h4')
-                for k in j:
-                    idr.append(k['href'].split('=')[-1][:-1])
-                l = i.text
-                n = re.findall('Station:[^"]*', l)
-                p = re.sub('Playing', '\nPlaying', n[0])
-                p=p.rstrip()
-                a = p.split('\n')
-                name.append(a[0].split(":")[1])
-                track.append(a[1].split(':')[1])
-                aformat.append(q.text)
-                listeners.append(g.text)
-                bitrate.append(z.text)
-            for i in range(len(idr)):
-                m.append(name[i].strip().replace('/', '-')+'-TV	id='+str(idr[i]).replace('\\', '')+'\nBitrate='+str(bitrate[i])+'\nListeners='+str(listeners[i])+'\n')
+            pass
         else:
             url = "https://www.shoutcast.com/Home/BrowseByGenre"
-            #content = ccurl(url, opt, 1)
             post = 'genrename='+opt
             content = ccurl(url+'#'+'-d'+'#'+post)
             m = self.process_page(content)
@@ -148,34 +99,18 @@ class Shoutcast():
     
     def getEpnList(self, name, opt, depth_list, extra_info, siteName, category):
         name_id = (re.search('id=[^|]*', extra_info).group()).split('=')[1]
-        #nm = name.rsplit('-', 1)
-        #name = nm[0]
-        #name_id = nm[1]
-        #name = nm[0]
         file_arr = []
         id_station = int(name_id)
         station_url = ''
         if opt == "TV" or '-TV' in name:
-            url = "http://thugie.nl/streams.php?tunein="+str(id_station)
-            #content = ccurl(url, '', 1)
-            content = ccurl(url)
-            final = re.findall('http://[^\n]*', content)
-            station_url = final[0].rstrip()
-            if 'stream.nsv' not in station_url:
-                #print "Hello" + station_url
-                station_url = str(station_url.rstrip()+";stream.nsv")
-            
-            
+            pass
         else:
             url = "https://www.shoutcast.com/Player/GetStreamUrl"
-            #content = ccurl(url, id_station, 2)
             post = 'station='+str(id_station)
             content = ccurl(url+'#-d#'+post)
             m = re.findall('http://[^"]*', content)
             station_url = str(m[0])
         file_arr.append(name+'	'+station_url+'	'+'NONE')
-        #file_arr.append('No.jpg')
-        #file_arr.append('Summary Not Available')
         record_history = True
         return (file_arr, 'Summary Not Available', 'No.jpg', record_history, depth_list)
         
