5a28b49b076202e66cf67e2fedb6437ab30cff7f
kanishka-linux <kanishka.linux@gmail.com>
2018-03-11 22:48:32 +0530

    version update

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
         ]
 setup(
     name='kawaii-player', 
-    version='3.2.0', 
+    version='3.3.0', 
     license='GPLv3', 
     author='kanishka-linux', 
     author_email='kanishka.linux@gmail.com', 
