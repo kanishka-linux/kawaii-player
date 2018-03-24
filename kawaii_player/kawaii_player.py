4ecd2fb3639fee25f175611545f738f4600c03c3
kanishka-linux <kanishka.linux@gmail.com>
2018-03-24 15:27:22 +0530

    version update

--- kawaii_player/kawaii_player.py
+++ kawaii_player/kawaii_player.py
@@ -1508,7 +1508,7 @@ watch/unwatch status")
         self.mplayer_timer = QtCore.QTimer()
         self.mplayer_timer.timeout.connect(self.mplayer_unpause)
         self.mplayer_timer.setSingleShot(True)
-        self.version_number = (3, 3, 0, 0)
+        self.version_number = (3, 3, 1, 1)
         self.threadPool = []
         self.threadPoolthumb = []
         self.thumbnail_cnt = 0
