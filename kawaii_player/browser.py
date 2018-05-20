7dfad0d60276451e76b59dcdc29a21b0c079ab08
kanishka-linux <kanishka.linux@gmail.com>
2018-05-20 15:53:45 +0530

    change season episode fetch method

--- kawaii_player/browser.py
+++ kawaii_player/browser.py
@@ -581,10 +581,18 @@ class Browser(QtWebEngineWidgets.QWebEngineView):
                 my_copy = self.ui.epn_arr_list.copy()
                 r = self.ui.list1.currentRow()
                 nm = self.ui.get_title_name(r)
-                self.ui.metaengine.getTvdbEpnInfo(
-                    url, site=self.site,
-                    epn_arr=my_copy, name=nm, row=r
-                    )
+                video_dir = None
+                if self.site.lower() == 'video':
+                    video_dir = self.ui.original_path_name[r].split('\t')[-1]
+                elif self.site.lower() in ['playlists', 'none', 'music']:
+                    pass
+                else:
+                    video_dir = self.ui.original_path_name[r]
+                self.ui.posterfound_new(
+                    name=nm, site=self.site, url=url, direct_url=True, 
+                    copy_summary=False, copy_poster=False, copy_fanart=False,
+                    get_sum=True, video_dir=video_dir
+                )
         elif (option.lower() == 'artist link' or option.lower() == 'series link' 
                 or option.lower() == 'series/movie link'):
             r = self.ui.list1.currentRow()
