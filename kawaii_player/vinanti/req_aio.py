300bb87e1fa5fcfdf6289f64eb1df5233bdd70bc
kanishka-linux <kanishka.linux@gmail.com>
2018-06-19 16:54:54 +0530

    check aiohttp

--- kawaii_player/vinanti/req_aio.py
+++ kawaii_player/vinanti/req_aio.py
@@ -19,7 +19,12 @@ along with vinanti.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
-import aiohttp
+
+try:
+    import aiohttp
+except ImportError:
+    pass
+    
 import asyncio
 import mimetypes
 
