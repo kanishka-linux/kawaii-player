df58751c1ee3bfc804132673992e16a3a6077e30
kanishka-linux <kanishka.linux@gmail.com>
2019-03-03 16:52:50 +0530

    update minor version number

--- kawaii_player/kawaii_player.py
+++ kawaii_player/kawaii_player.py
@@ -1398,7 +1398,7 @@ class Ui_MainWindow(object):
         self.mplayer_timer = QtCore.QTimer()
         self.mplayer_timer.timeout.connect(self.mplayer_unpause)
         self.mplayer_timer.setSingleShot(True)
-        self.version_number = (3, 9, 0, 1)
+        self.version_number = (3, 9, 1, 1)
         self.threadPool = []
         self.threadPoolthumb = []
         self.thumbnail_cnt = 0
