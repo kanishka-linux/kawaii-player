b3ab164ac5d37f2c56e3100d13d0014509fd0cad
kanishka-linux <kanishka.linux@gmail.com>
2019-03-28 05:19:47 +0530

    set locale

--- kawaii_player/mpv_opengl.py
+++ kawaii_player/mpv_opengl.py
@@ -5,6 +5,7 @@
 """
 import os
 import platform
+import locale
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt, QMetaObject, pyqtSlot
 from PyQt5.QtWidgets import QOpenGLWidget, QApplication
@@ -112,6 +113,7 @@ class MpvOpenglWidget(QOpenGLWidget):
             self.args_dict.update({"ao":"wasapi"})
         if gui.mpvplayer_string_list and gui.use_custom_config_file:
             self.create_args_dict()
+        locale.setlocale(locale.LC_NUMERIC, 'C')
         self.mpv = MPV(**self.args_dict)
         self.mpv.observe_property("time-pos", self.time_observer)
         self.mpv.observe_property("eof-reached", self.eof_observer)
