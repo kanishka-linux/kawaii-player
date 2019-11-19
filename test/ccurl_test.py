7d7035675375d0d18d925954c76cfe63019a13b1
Francis Piche <francisplanning@gmail.com>
2019-11-19 14:15:34 -0500

    Basic tox setup, make tests pass

--- test/ccurl_test.py
+++ test/ccurl_test.py
@@ -1,11 +1,20 @@
 import unittest
 import sys
 import os
+from get_functions import ccurl, ccurlCmd, ccurlWget
 
 class TestCcurl(unittest.TestCase):
     
     url = 'https://www.google.com'
     
+    def setUp(self):
+        BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
+        parent_basedir, __ = os.path.split(BASEDIR)
+        print(parent_basedir)
+        sys.path.insert(0, parent_basedir)
+        k_dir = os.path.join(parent_basedir, 'kawaii_player')
+        sys.path.insert(0, k_dir)
+        
     def test_curl(self):
         self.assertTrue(ccurl(self.url))
     
@@ -16,11 +25,5 @@ class TestCcurl(unittest.TestCase):
         self.assertTrue(ccurlWget(self.url))
         
 if __name__ == '__main__':
-    BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
-    parent_basedir, __ = os.path.split(BASEDIR)
-    print(parent_basedir)
-    sys.path.insert(0, parent_basedir)
-    k_dir = os.path.join(parent_basedir, 'kawaii_player')
-    sys.path.insert(0, k_dir)
-    from get_functions import ccurl, ccurlCmd, ccurlWget
+    
     unittest.main()
--- test/write_test.py
+++ test/write_test.py
@@ -3,7 +3,26 @@ import sys
 import os
 import time
 
+from player_functions import write_files, get_home_dir
+
 class TestWrite(unittest.TestCase):
+    
+    def setUp(self):
+        global home, test_file
+        BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
+        parent_basedir, __ = os.path.split(BASEDIR)
+        sys.path.insert(0, parent_basedir)
+        k_dir = os.path.join(parent_basedir, 'kawaii_player')
+        sys.path.insert(0, k_dir)
+
+        home = get_home_dir(mode='test')
+        if not os.path.exists(home):
+            os.makedirs(home)
+        tmp_dir = os.path.join(home, 'tmp')
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
+        test_file = os.path.join(tmp_dir, 'write_test.txt')
+        print(test_file)
     
     def test_write_line_by_line_list(self):
         content = ['This', 'is', 'Test', 'Case']
@@ -18,19 +37,4 @@ class TestWrite(unittest.TestCase):
         self.assertIsNone(write_files(test_file, content, line_by_line=False, mode='test'))
     
 if __name__ == '__main__':
-    global home, test_file
-    BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
-    parent_basedir, __ = os.path.split(BASEDIR)
-    sys.path.insert(0, parent_basedir)
-    k_dir = os.path.join(parent_basedir, 'kawaii_player')
-    sys.path.insert(0, k_dir)
-    from player_functions import write_files, get_home_dir
-    home = get_home_dir(mode='test')
-    if not os.path.exists(home):
-        os.makedirs(home)
-    tmp_dir = os.path.join(home, 'tmp')
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-    test_file = os.path.join(tmp_dir, 'write_test.txt')
-    print(test_file)
     unittest.main()
