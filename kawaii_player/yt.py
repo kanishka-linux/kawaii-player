9df1fd9f59b464362512df0297191bc77fc1ec2b
kanishka-linux <kanishka.linux@gmail.com>
2023-10-24 01:18:35 +0530

    refactor yt.py

--- kawaii_player/yt.py
+++ kawaii_player/yt.py
@@ -116,6 +116,7 @@ class YTDL:
                 else:
                     res = 4000
                 if (quality == 'best' and ytdl_path == 'default'
+                        and self.ui.player_val != "libvlc"
                         and (reqfrom is None or reqfrom == 'desktop')
                         and not self.ui.gapless_network_stream):
                     if ytdl_extra:
@@ -185,8 +186,6 @@ class YTDL:
         else:
             content = subprocess.check_output(ytdl_list, stderr= subprocess.STDOUT, shell=True)
         content = str(content, 'utf-8')
-        if not content.startswith('{'):
-            content = re.sub('[^\{]*', '', content, 1)
         audio_dict = {}
         video_dict = {}
         best = None
@@ -195,69 +194,41 @@ class YTDL:
         best = js.get('format_id')
         subs = js.get('subtitles')
         title = js.get('fulltitle')
-        if not subs:
-            subs = {}
-        for i, j in js.items():
-            if isinstance(j, list):
-                for k, l in enumerate(j):
-                    if isinstance(l, dict):
-                        attr_list = [
-                            l.get('width'), l.get('height'),
-                            l.get('ext'), l.get('abr'),
-                            l.get('url')
-                            ]
-                        if l.get('width'):
-                            video_dict.update({l.get('format_id'): attr_list.copy()})
-                        else:
-                            audio_dict.update({l.get('format_id'):attr_list.copy()})
-        video_sort = [j for i, j in video_dict.items()]
-        video_sort = sorted(video_sort, key=lambda x: int(x[1]), reverse=True)
-        audio_sort = [j for i, j in audio_dict.items() if j[3] is not None]
-        audio_sort = sorted(audio_sort, key=lambda x: int(x[3]), reverse=True)
-        req_vid = None
-        req_aud = None
-        if audio_sort:
-            req_aud = audio_sort[0]
-        for i in video_sort:
-            if res >= int(i[1]):
-                req_vid = i
-                break
+        video_plus_audio = list(
+                    filter(lambda x: x.get("acodec") != "none" and x.get("vcodec") != "none" and x.get("height") <= res, js.get("formats"))
+                )
+        audio_only = list(
+                    filter(lambda x: x.get("acodec") != "none" and x.get("vcodec") == "none" and x.get("manifest_url") == None, js.get("formats"))
+                )
+        video_only = list(
+                    filter(lambda x: x.get("acodec") == "none" and x.get("vcodec") != "none" and x.get("manifest_url") == None and x.get("height") <= res, js.get("formats"))
+                )
+        video_only = sorted(video_only, key=lambda x: x.get("height"), reverse=True)
+        audio_only = sorted(audio_only, key=lambda x: x.get("quality"), reverse=True)
+        video_plus_audio = sorted(video_plus_audio, key=lambda x: x.get("height"), reverse=True)
+
+        subs_dict = dict([(k, list(filter(lambda x: x.get("ext") == "vtt", v))) for k, v in subs.items()])
+
         ytid = js.get('id')
         sub_arr = []
         if sub_folder and not os.path.exists(sub_folder):
             os.makedirs(sub_folder)
-        for key, value in subs.items():
-            if isinstance(value, list):
-                for entry in value:
-                    if isinstance(entry, dict):
-                        urlsub = entry.get('url')
-                        ext = entry.get('ext')
-                        if ext != 'ttml':
-                            path = '{}.{}.{}'.format(ytid, key, ext)
-                            sub_path = os.path.join(sub_folder, path)
-                            sub_arr.append([urlsub, sub_path, title])
-        final_url_vid = req_vid[-1]
-        final_url_aud = req_aud[-1]
-        if final_url_vid and final_url_aud:
-            final_url = final_url_vid.strip() + '::' + final_url_aud.strip()
-        elif final_url_vid:
-            final_url = final_url_vid.strip()
-        if mode == 'music':
-            final_url = final_url_aud.strip()
-            logger.debug('\n only audio required since music mode has been selected \n')
-        elif mode == 'offline':
-            final_url = '"{}"::"{}"::{}'.format(youtube_dl, url, "-o")
-        if (sub_arr and self.ui.mpvplayer_val.processId() == 0) or not self.ui.gapless_network_stream:
-            self.ui.gui_signals.subtitle_fetch(sub_arr)
-        elif sub_arr and self.ui.gapless_network_stream:
-            for sub in sub_arr:
-                url, sub_path, title = sub
-                if not os.path.isfile(sub_path):
-                    self.vnt.get(url, out=sub_path)
-                final_url = final_url + '::' + sub_path
-                logger.debug(sub_path)
-        if final_url.startswith("https://manifest."):
-            final_url = url
+        for key, value in subs_dict.items():
+            if len(value) > 0:
+                urlsub = value[0].get("url")
+                ext = value[0].get('ext')
+                path = '{}.{}.{}'.format(ytid, key, ext)
+                sub_path = os.path.join(sub_folder, path)
+                sub_arr.append([urlsub, sub_path, title])
+
+        self.ui.gui_signals.subtitle_fetch(sub_arr)
+        final_url = video_only[0].get("url") + "::" + audio_only[0].get("url")
+        if "en" in subs_dict.keys():
+            en_subs = subs_dict["en"]
+            if len(en_subs) > 0:
+                sub_url = en_subs[0].get("url")
+                final_url = final_url + "::" + sub_url
+
         return final_url
 
     def post_subtitle_fetch(self, *args):
@@ -265,4 +236,3 @@ class YTDL:
         title = args[0]
         if result and result.out_file:
             self.ui.gui_signals.subtitle_apply(result.out_file, title)
-    
