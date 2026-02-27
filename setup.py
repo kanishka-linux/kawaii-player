e3b0dc2ff3676a9e3bffaab5052efd103868233f
kanishka-linux <kanishka.linux@gmail.com>
2026-02-27 17:42:34 +0530

    remove pycurl and certifi dependencies

--- setup.py
+++ setup.py
@@ -36,13 +36,11 @@ install_requires = []
 if system in ["darwin", "nt"]:
     install_requires = [
         "PyQt6",
-        "pycurl",
         "bs4",
         "Pillow",
         "mutagen",
         "lxml",
         "yt-dlp",
-        "certifi",
         "PyQt6-WebEngine",
         "PyOpenGL",
         "python-vlc"
@@ -68,9 +66,11 @@ setup(
     url='https://github.com/kanishka-linux/kawaii-player',
     long_description="README.md",
     packages=[
-        'kawaii_player', 'kawaii_player.Plugins', 'kawaii_player.widgets',
-        'kawaii_player.hls_webengine', 'kawaii_player.hls_webkit',
-        'kawaii_player.vinanti', 'kawaii_player.tvdb_async',
+        'kawaii_player',
+        'kawaii_player.Plugins',
+        'kawaii_player.widgets',
+        'kawaii_player.hls_webengine',
+        'kawaii_player.vinanti',
         'kawaii_player.series_info_fetcher'
         ],
     include_package_data=True,
